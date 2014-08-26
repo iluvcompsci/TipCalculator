913f5180b7f6e1c6cbf58a48816c27825cdf6645
iluvcompsci <bri.chapman@gmail.com>
2014-08-26 09:40:55 -0500

    finished with the demo portion

--- TipCalculator/BCCViewController.m
+++ TipCalculator/BCCViewController.m
@@ -8,16 +8,25 @@
 
 #import "BCCViewController.h"
 
-@interface BCCViewController ()
-
+@interface BCCViewController () <UITextFieldDelegate>
+@property (weak, nonatomic) IBOutlet UITextField *checkTotal;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property NSInteger percentTip;
 @end
 
 @implementation BCCViewController
+- (IBAction)textFieldChanged:(UITextField *)sender {
+    float check = [self.checkTotal.text floatValue];
+    float tip = check * ((float)self.percentTip / 100.0);
+    self.tipLabel.text = [NSString stringWithFormat:@"%.2f", tip];
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [self.checkTotal becomeFirstResponder];
+    self.percentTip = 15;
 }
 
 - (void)didReceiveMemoryWarning
